a8c2e005dcd85a2eddbd57df83b0723653eabead
Aibek-S <ajbeksuundik@gmail.com>
2025-09-06 12:42:07 +0500

    fix vite base for deploy

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: ["670b2246c547.ngrok-free.app"],
-    host: true,
-  },
+    plugins: [react()],
+    base: "./",
 });
